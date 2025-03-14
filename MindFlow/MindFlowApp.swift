13ae9fb7dedf1d06e073919eae3c02c297b545de
Sharnabh Banerjee <banerjeesharnabh@gmail.com>
2025-03-14 11:08:10 +0530

    Initial Commit

---
+++ MindFlow/MindFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindFlowApp.swift
+//  MindFlow
+//
+//  Created by Sharnabh on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MindFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
